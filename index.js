590554159499e44e8f293795348910f7941840bb
Ethan <ethanmontera@gmail.com>
2023-10-15 15:48:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-function sendMail() {
-    var params = {
-        name: document.getElementById("email").value,
-    };
 
-    const serviceID = "service_9i6b9do";
-    const templateID = "template_asiiaag";
-
-    emailjs
-        .send(serviceID, templateID, params)
-        .then((res) => {
-            document.getElementById("email").value = "";
-
-            console.log(res);
-            alert("Your Message was Sent Successfully");
-        })
-
-    .catch((err) => console.log(err));
-}
 
 
 // constants
